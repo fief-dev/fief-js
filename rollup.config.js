bd2fc115ff54208264366d969a3cf83a6319f60e
François Voron <fvoron@gmail.com>
2022-04-05 11:13:26 +0200

    Add terser minification to browser build

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
 
 import pkg from './package.json';
 
@@ -20,7 +21,8 @@ export default [
       json(),
       commonjs(),
       typescript(),
-    ]
+      terser(),
+    ],
   },
   // CommonJS (for Node) and ES module (for bundlers) build.
   {
@@ -32,6 +34,6 @@ export default [
     output: [
       { file: pkg.main, format: 'cjs', sourcemap: true },
       { file: pkg.module, format: 'es', sourcemap: true },
-    ]
-  }
+    ],
+  },
 ];
