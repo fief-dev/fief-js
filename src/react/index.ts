f717624669c1767485b6632f35d8d6e75d6dc44b
François Voron <fvoron@gmail.com>
2022-04-06 17:45:22 +0200

    Fix missing hooks exports

--- src/react/index.ts
+++ src/react/index.ts
@@ -1,10 +1,17 @@
 import { FiefAuthContext } from './context';
 import { FiefAuthProvider } from './provider';
-import { useFiefAuth, useFiefIsAuthenticated } from './hooks';
+import {
+  useFiefAuth,
+  useFiefIsAuthenticated,
+  useFiefTokenInfo,
+  useFiefUserinfo,
+} from './hooks';
 
 export {
   FiefAuthContext,
   FiefAuthProvider,
   useFiefAuth,
   useFiefIsAuthenticated,
+  useFiefTokenInfo,
+  useFiefUserinfo,
 };
