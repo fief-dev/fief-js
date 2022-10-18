05e3c16ab45942386314ad8bd310e02d0edc6610
François Voron <fvoron@gmail.com>
2022-10-18 13:10:08 +0200

    Fix unit tests for Node 16 and 17

---
+++ tests/globalSetup.js
@@ -0,0 +1,13 @@
+/**
+ * In Node 16 and 17, Fetch API is not available globally,
+ * which is required to run NextJS unit tests.
+ *
+ * To circumvent this, we export manually the versions from node-fetch.
+ */
+module.exports = async (_globalConfig, _projectConfig) => {
+  // eslint-disable-next-line global-require
+  const { Headers, Request, Response } = require('node-fetch');
+  globalThis.Headers = Headers;
+  globalThis.Request = Request;
+  globalThis.Response = Response;
+};
